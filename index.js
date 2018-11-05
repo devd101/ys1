8cbff1f56ad5dda03b213e21a95d07eddc77c485
devd101 <devd101@protonmail.com>
2018-11-05 12:40:26 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,159 @@
+/*
+	The following envionment variables must be set:
+
+	botToken
+	yedionUrl
+	yoasimUrl
+	yoasimPTUrl
+
+*/
+
+const Ttoken = process.env.botToken;
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const session = require('telegraf/session')
+
+
+const fs = require('fs');
+const parseString = require('xml2js').parseString;
+let xmlTxt = fs.readFileSync('./text/textData.xml', 'utf8');
+let txtData;
+parseString(xmlTxt, { trim: true, explicitArray: false }, (err, result) => {
+	txtData = result.root;
+	// console.log(txtData);
+});
+
+
+const bot = new Telegraf(Ttoken)
+
+
+const mainKeyboard = Markup.inlineKeyboard([
+	// Markup.urlButton('❤️', 'http://telegraf.js.org'),
+
+	// Markup.urlButton('ידיעון', 'https://t.me/Yoasim'),
+	Markup.urlButton('ידיעון', process.env.yedionUrl),
+
+	Markup.callbackButton('שאלות ותשובות', 'qna1'),
+
+	Markup.callbackButton('תרפיה ואינטגרציה', 'terapia'),
+	Markup.callbackButton('תרומה/עזרה', 'truma')
+],
+	{
+		columns: 1
+	})
+
+bot.start((ctx) => {
+	ctx.reply('ברוכים הבאים לפרויקט יוע"סים!', Extra.markup(mainKeyboard));
+});
+
+
+bot.action('restart',(ctx) => {
+	ctx.editMessageText('ברוכים הבאים לפרויקט יוע"סים!', Extra.markup(mainKeyboard));
+});
+
+// ******************************** QnA ********************************
+
+bot.action('qna1', (ctx) => {
+	ctx.editMessageText(txtData.qna1,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				Markup.callbackButton('המשך...', 'qna2'),
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+bot.action('qna2', (ctx) => {
+	ctx.reply(txtData.qna2,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				Markup.callbackButton('המשך...', 'qna3')
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+bot.action('qna3', (ctx) => {
+	ctx.reply(txtData.qna3,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				Markup.callbackButton('המשך...', 'qna4')
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+bot.action('qna4', (ctx) => {
+	ctx.reply(txtData.qna4,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				// Markup.urlButton('אני מבין וקראתי את החוקים', 'https://t.me/Yoasim'),
+				Markup.urlButton('אני מבין וקראתי את החוקים', process.env.yoasimUrl),
+				Markup.callbackButton('לתפריט הראשי', 'restart'),
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+bot.action('terapia', (ctx) => {
+	ctx.editMessageText(txtData.terapia,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				// Markup.urlButton('אני מבין וקראתי את החוקים', 'https://t.me/YoasimPT'),
+				Markup.urlButton('אני מבין וקראתי את החוקים', process.env.yoasimPTUrl),
+				Markup.callbackButton('לתפריט הראשי', 'restart'),
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+
+bot.action('truma', (ctx) => {
+	ctx.editMessageText(txtData.truma,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				Markup.callbackButton('כתובת הארנק', 'walletAddress'),
+				Markup.callbackButton('לתפריט הראשי', 'restart'),
+			],
+			{
+				columns: 1
+			})
+		)
+	)
+});
+
+bot.action('walletAddress', (ctx) => {
+	// ctx.reply('1A4nD24t2iwzYkT8dBZJ2sna5SBtNd2YVx');	
+	ctx.editMessageText(txtData.walletAddress,
+		Extra.markup(
+			Markup.inlineKeyboard([
+				Markup.callbackButton('לתפריט הראשי', 'restart'),
+			],
+			{
+				columns: 1
+			})
+		)
+	)	
+});
+
+bot.startPolling();
