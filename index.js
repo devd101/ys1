6d6ef204584487e139cb013c1118e5749b749802
devd101 <devd101@protonmail.com>
2018-11-11 10:48:08 +0200

    Fix + wallet QR

--- index.js
+++ index.js
@@ -8,7 +8,16 @@
 
 */
 
-const Ttoken = process.env.botToken;
+let devConfig = {};
+var fs = require('fs');
+if (fs.existsSync('./devConfig.js')) {
+	devConfig = require('./devConfig.js');
+	//console.log('devConfig: ', devConfig);
+}
+
+// process.exit(0);
+
+const Ttoken = devConfig.botToken || process.env.botToken;
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
@@ -16,7 +25,6 @@ const Markup = require('telegraf/markup')
 const session = require('telegraf/session')
 
 
-const fs = require('fs');
 const parseString = require('xml2js').parseString;
 let xmlTxt = fs.readFileSync('./text/textData.xml', 'utf8');
 let txtData;
@@ -33,7 +41,7 @@ const mainKeyboard = Markup.inlineKeyboard([
 	// Markup.urlButton('❤️', 'http://telegraf.js.org'),
 
 	// Markup.urlButton('ידיעון', 'https://t.me/Yoasim'),
-	Markup.urlButton('ידיעון', process.env.yedionUrl),
+	Markup.urlButton('ידיעון', devConfig.yedionUrl || process.env.yedionUrl),
 
 	Markup.callbackButton('שאלות ותשובות', 'qna1'),
 
@@ -49,9 +57,16 @@ bot.start((ctx) => {
 });
 
 
-bot.action('restart',(ctx) => {
+bot.action('restart', (ctx) => {
 	ctx.editMessageText('ברוכים הבאים לפרויקט יוע"סים!', Extra.markup(mainKeyboard));
 });
+
+//Quick and (very) dirty....
+bot.action('restartNoEdit', (ctx) => {
+	ctx.reply('ברוכים הבאים לפרויקט יוע"סים!', Extra.markup(mainKeyboard));
+});
+
+
 
 // ******************************** QnA ********************************
 
@@ -61,9 +76,9 @@ bot.action('qna1', (ctx) => {
 			Markup.inlineKeyboard([
 				Markup.callbackButton('המשך...', 'qna2'),
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
@@ -74,9 +89,9 @@ bot.action('qna2', (ctx) => {
 			Markup.inlineKeyboard([
 				Markup.callbackButton('המשך...', 'qna3')
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
@@ -87,9 +102,9 @@ bot.action('qna3', (ctx) => {
 			Markup.inlineKeyboard([
 				Markup.callbackButton('המשך...', 'qna4')
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
@@ -99,12 +114,12 @@ bot.action('qna4', (ctx) => {
 		Extra.markup(
 			Markup.inlineKeyboard([
 				// Markup.urlButton('אני מבין וקראתי את החוקים', 'https://t.me/Yoasim'),
-				Markup.urlButton('אני מבין וקראתי את החוקים', process.env.yoasimUrl),
+				Markup.urlButton('אני מבין וקראתי את החוקים', devConfig.yoasimUrl || process.env.yoasimUrl),
 				Markup.callbackButton('לתפריט הראשי', 'restart'),
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
@@ -117,12 +132,12 @@ bot.action('terapia', (ctx) => {
 		Extra.markup(
 			Markup.inlineKeyboard([
 				// Markup.urlButton('אני מבין וקראתי את החוקים', 'https://t.me/YoasimPT'),
-				Markup.urlButton('אני מבין וקראתי את החוקים', process.env.yoasimPTUrl),
+				Markup.urlButton('אני מבין וקראתי את החוקים', devConfig.yoasimPTUrl || process.env.yoasimPTUrl),
 				Markup.callbackButton('לתפריט הראשי', 'restart'),
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
@@ -135,25 +150,37 @@ bot.action('truma', (ctx) => {
 				Markup.callbackButton('כתובת הארנק', 'walletAddress'),
 				Markup.callbackButton('לתפריט הראשי', 'restart'),
 			],
-			{
-				columns: 1
-			})
+				{
+					columns: 1
+				})
 		)
 	)
 });
 
 bot.action('walletAddress', (ctx) => {
 	// ctx.reply('1A4nD24t2iwzYkT8dBZJ2sna5SBtNd2YVx');	
-	ctx.editMessageText(txtData.walletAddress,
+	ctx.reply(txtData.walletAddress);
+
+	ctx.replyWithPhoto({ source: fs.createReadStream('./img/walletQR.jpg') },
 		Extra.markup(
 			Markup.inlineKeyboard([
-				Markup.callbackButton('לתפריט הראשי', 'restart'),
+				Markup.callbackButton('לתפריט הראשי', 'restartNoEdit'),
 			],
-			{
-				columns: 1
-			})
-		)
-	)	
+				{
+					columns: 1
+				})
+		));
+
+	// ctx.editMessageText(txtData.walletAddress,
+	// 	Extra.markup(
+	// 		Markup.inlineKeyboard([
+	// 			Markup.callbackButton('לתפריט הראשי', 'restart'),
+	// 		],
+	// 			{
+	// 				columns: 1
+	// 			})
+	// 	)
+	// )
 });
 
 bot.startPolling();
